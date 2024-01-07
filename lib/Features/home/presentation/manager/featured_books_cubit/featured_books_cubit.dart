aba7ba0e8b8221ac2c48d661a763ad55b1ff4d09
Myproject <mohamed.alaa@gmail.com>
2024-01-07 11:56:02 +0200

    implement fetch featured cubit

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,6 +1,24 @@
-import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featuered_books_use_case.dart';
 import 'package:bookly/Features/home/presentation/manager/featured_books_cubit/featured_books_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeatueredBooksUseCase)
+      : super(FeaturedBooksInitial());
+  final FetchFeatueredBooksUseCase fetchFeatueredBooksUseCase;
+
+  static FeaturedBooksCubit get(context) => BlocProvider.of(context);
+
+  List<BookEntity> booksList = [];
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await fetchFeatueredBooksUseCase.call();
+    result.fold((failure) {
+      emit(FeaturedBooksFailuer(errorMessage: failure.message));
+    }, (success) {
+      emit(FeaturedBooksSuccess());
+      booksList.addAll(success);
+    });
+  }
 }
